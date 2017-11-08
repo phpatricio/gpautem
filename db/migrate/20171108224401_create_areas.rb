348beadeb28b175ee62fa90e8ef262a2bcac3501
lucianoutem <lucianopjta@gmail.com>
2017-11-08 19:59:18 -0300

    se creo el CRUD de controlador Area

--- db/migrate/20171108224401_create_areas.rb
+++ db/migrate/20171108224401_create_areas.rb
@@ -1,7 +1,7 @@
 class CreateAreas < ActiveRecord::Migration[5.0]
   def change
     create_table :areas do |t|
-      t.string :Nombre
+      t.string :nombre
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20171105235144) do
-
+ActiveRecord::Schema.define(version: 20171108224401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +27,7 @@ ActiveRecord::Schema.define(version: 20171105235144) do
   end
 
   create_table "areas", force: :cascade do |t|
-    t.string   "Nombre"
+    t.string   "nombre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
