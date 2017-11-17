e07ddb9c603ef312f479d7227f724953daa77f0f
lucianoutem <lucianopjta@gmail.com>
2017-11-17 17:31:02 -0300

    se agrego gema para comparar fecha inicio y termino

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'adminlte-generators'
 gem 'devise'
 gem 'will_paginate', '>= 3.1'
 gem 'rut_validation'
+gem 'date_validator'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/models/practica.rb
+++ app/models/practica.rb
@@ -1,4 +1,8 @@
 class Practica < ApplicationRecord
   has_one :profesional_guia
   validates :descripcion_p, presence: true
+  validates :fecha_inicio, date: true
+  validates :fecha_termino, date: true
+  validates :fecha_inicio, date: { before: :fecha_termino }
+
 end
