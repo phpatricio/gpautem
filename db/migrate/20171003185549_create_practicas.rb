7048cdefbc3d9583f2996a14552fc2386c5f2cfd
patricio <patriciocabreravera@gmail.com>
2017-11-21 19:37:32 -0300

    Se agrego validaciones con errores evaluacion

--- db/migrate/20171003185549_create_practicas.rb
+++ db/migrate/20171003185549_create_practicas.rb
@@ -5,8 +5,8 @@ class CreatePracticas < ActiveRecord::Migration[5.0]
       t.date :fecha_termino
       t.text :descripcion_p
       t.references :alumno, foreign_key: true
-      t.references :profesional_guia
-
+      t.references :profesional_guia, foreign_key: true
+      t.references :evaluacion,foreign_key: true
       t.timestamps
     end
   end
