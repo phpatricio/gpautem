93637ba7510fc6d9a7722b04b14fa9703075f2b2
patricio <patriciocabreravera@gmail.com>
2017-11-22 03:34:33 -0300

    Se mejoro vista convenio y empresa

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -1,8 +1,3 @@
 class Empresa < ApplicationRecord
-	has_many :practica
-	has_many :conveio
-	validates_presence_of :rut, message: "por favor complete el espacio en blanco (campo obligatorio)"
-	validates :rut_empresa, rut: true
-	validates_uniqueness_of :rut, message: "el rut ungresado ya esta en el sistema"
-    validates_presence_of :nombre, message: "por favor complete el espacio en blanco (campo obligatorio)"
+	
 end
