a9329f0d05fd5352e9473061427226f63b62ff46
patricio <patriciocabreravera@gmail.com>
2017-12-13 01:47:26 -0300

    Se ingreso validacion front para alumno secretaria y demas cambios

--- app/models/seguimiento.rb
+++ app/models/seguimiento.rb
@@ -1,11 +1,20 @@
 class Seguimiento < ApplicationRecord
-	has_one :practica
-	validates :fecha_1, date: true
+	has_one :practica, dependent: :destroy
+	#validates :fecha_1, date: true
 	validates_presence_of :comentario_visita_1, message: ": por fa2or complete el espacio en blanco (campo obligatorio)"
-	validates :fecha_2, date: true
+	#validates :fecha_2, date: true
 	validates_presence_of :comentario_visita_2, message: ": por favor complete el espacio en blanco (campo obligatorio)"
-	validates :fecha_3, date: true
+	#validates :fecha_3, date: true
 	validates_presence_of :comentario_visita_3, message: ": por favor complete el espacio en blanco (campo obligatorio)"
-	validates :fecha_1, date: { before: :fecha_2 }
-	validates :fecha_2, date: { before: :fecha_3 }
+	validate :fecha_correcta
+	
+	def fecha_correcta
+		
+
+			if fecha_1 < fecha_2
+			  errors.add(:fecha_1, "La fecha de termino no puede ser inferior a la fecha de inicio")
+			end
+
+			
+	end
 end
